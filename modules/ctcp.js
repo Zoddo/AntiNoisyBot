08ae0c489b7555628043a38671e932ab9996c02e
Zoddo <zoddo.ino@gmail.com>
2016-04-16 23:23:20 +0200

    Add CTCP TIME & SOURCE support

--- modules/ctcp.js
+++ modules/ctcp.js
@@ -2,16 +2,31 @@ var module = require('../objects/module.js').module;
 var ctcp = new module('ctcp');
 
 ctcp.load = function() {
-	client.on('ctcp-version', onVersion);
+	client.on('ctcp-privmsg', onCTCP);
 };
 
 ctcp.unload = function() {
-	client.removeListener('ctcp-version', onVersion);
+	client.removeListener('ctcp-privmsg', onCTCP);
 };
 
-function onVersion(from)
+function onCTCP(from, to, text)
 {
-	client.ctcp(from, 'notice', 'VERSION AntiNoisyBot by Zoddo | An IRC bot to prevent noisy (quit/join flood due to unstable connection, etc.) | https://github.com/Zoddo/AntiNoisy');
+	var args = text.split(' ');
+	var command = args.shift().toUpperCase();
+
+	switch (command) {
+		case 'TIME':
+			client.ctcp(from, 'notice', 'TIME ' + Date());
+			break;
+
+		case 'VERSION':
+			client.ctcp(from, 'notice', 'VERSION AntiNoisyBot by Zoddo | An IRC bot to prevent noisy (quit/join flood due to unstable connection, etc.) | https://github.com/Zoddo/AntiNoisy');
+			break;
+
+		case 'SOURCE':
+			client.ctcp(from, 'notice', 'SOURCE https://github.com/Zoddo/AntiNoisy');
+			break;
+	}
 }
 
 exports.module = ctcp;
